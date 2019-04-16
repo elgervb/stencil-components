9d9ef5303d12fa0fb978a118a87541a41651ef24
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2019-04-16 21:58:26 +0200

    flyout component

---
+++ src/components/flyout/evb-flyout.tsx
@@ -0,0 +1,54 @@
+import { Component, ComponentInterface, Event, EventEmitter, Listen, Method, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'evb-flyout',
+  shadow: true,
+  styleUrl: 'evb-flyout.css'
+})
+export class EvbFlyout implements ComponentInterface {
+
+  @Prop({ mutable: true }) open: boolean;
+  @Prop() header: string;
+
+  @Event({ eventName: 'close' }) onClose: EventEmitter<void>;
+  @Event({ eventName: 'open' }) onOpen: EventEmitter<void>;
+
+  @Listen('window:keydown')
+  close(event: KeyboardEvent) {
+    if (this.open && event.key === 'Escape') {
+      this.toggle(false);
+    }
+  }
+
+  @Method()
+  toggle(forceOpen?: boolean) {
+    this.open = forceOpen !== undefined ? forceOpen : !this.open;
+
+    this.open ? this.onOpen.emit() : this.onClose.emit();
+  }
+
+  hostData() {
+    return {
+      class: {
+        'flyout--open': this.open
+      }
+    };
+  }
+
+  render() {
+    if (!this.open) {
+      return '';
+    }
+    const heading = <evb-header heading='3'>{this.header}</evb-header>;
+
+    return (
+      <aside class='flyout'>
+        <header>
+          {heading}
+          <button class='close' onClick={() => this.open = false}>&times;</button>
+        </header>
+        <slot />
+      </aside>
+    );
+  }
+}
---
+++ src/components/flyout/readme.md
@@ -0,0 +1,45 @@
+# evb-flyout
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property | Attribute | Description | Type      | Default     |
+| -------- | --------- | ----------- | --------- | ----------- |
+| `header` | `header`  |             | `string`  | `undefined` |
+| `open`   | `open`    |             | `boolean` | `undefined` |
+
+
+## Events
+
+| Event   | Description | Type                |
+| ------- | ----------- | ------------------- |
+| `close` |             | `CustomEvent<void>` |
+| `open`  |             | `CustomEvent<void>` |
+
+
+## Methods
+
+### `toggle(forceOpen?: boolean) => void`
+
+
+
+#### Parameters
+
+| Name        | Type      | Description |
+| ----------- | --------- | ----------- |
+| `forceOpen` | `boolean` |             |
+
+#### Returns
+
+Type: `void`
+
+
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
